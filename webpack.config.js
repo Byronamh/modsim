f406102e2ef504f2d741acd99fddaa154a2591c6
Byronamh <byronamh@gmail.com>
2021-07-16 18:08:43 -0600

    feature: Added webpack and project structure along with dependencies

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const directoryPath = path.join(__dirname, '/src/labs');
+
+
+const folderScanPromise = new Promise((res, rej) => {
+    const chunks = [];
+    const entries = {}
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error('Unable to scan directory: ' + err);
+            rej(err);
+        }
+        //listing all files using forEach
+        files.forEach(folder => {
+
+            chunks.push(new HtmlWebpackPlugin({
+                filename: `${folder}/index.html`,
+                template: path.join(directoryPath, `${folder}/index.html`),
+                chunks: [folder]
+            }))
+            entries[folder] = path.join(directoryPath, `${folder}/index.js`)
+        });
+        res({chunks, entries})
+    });
+})
+
+module.exports = () => folderScanPromise.then(({chunks, entries}) => {
+    return {
+        entry: entries,
+        output: {
+            filename: './[name]/index.js',
+        },
+        plugins: [
+            ...chunks,
+            new HtmlWebpackPlugin({
+                filename: './index.html',
+                template: path.join(__dirname, '/src/static/index.html'),
+                chunks:['main']
+            })
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000,
+            open: true
+        },
+    }
+})
